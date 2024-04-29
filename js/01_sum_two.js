7b367a10e203fca4cad6b6a6489aa2b724ec8941
divyav26 <divyam2633@gmail.com>
2024-04-29 18:04:30 +0530

    basic program

---
+++ js/01_sum_two.js
@@ -0,0 +1,8 @@
+// Write a JavaScript function to calculate the sum of two numbers.
+
+const sum_num = (num1,num2)=>{
+    return num1+num2
+}
+const num1 = parseInt(prompt("Enetr the first number: "))
+const num2 = parseInt(prompt("Enter the second numbar : "))
+console.log(sum_num(4,5))
